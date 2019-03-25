ca471c2b68bcf8ea35e80f34aa3b1850f2bccccb
Pratik Kanungo <47408448+pk111297@users.noreply.github.com>
2019-03-25 11:02:01 +0530

    Create client.py

---
+++ testcases/client.py
@@ -0,0 +1,9 @@
+import asyncio
+import websockets
+async def test():
+    async with websockets.connect('ws://localhost:10000') as websocket:
+        await websocket.send("hello")
+        response = await websocket.recv()
+        print(response)
+asyncio.get_event_loop().run_until_complete(test())
+
